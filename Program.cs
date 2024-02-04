ea90cff614d228fb91fc06e058b9b880c4757612
Kayki Letieri <kaykiletieri37@gmail.com>
2024-02-04 19:51:12 -0300

    feat: finish suppliers endpoints

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ app.MapGet("/suppliers/{id}", async (
             ? Results.Ok(supplier) 
             : Results.NotFound())
     .Produces<Supplier>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
     .WithName("GetSupplierById")
     .WithTags("Suppliers");
 
@@ -54,35 +55,45 @@ app.MapPost("/suppliers", async (
     .Produces(StatusCodes.Status400BadRequest)
     .WithName("CreateSupplier")
     .WithTags("Suppliers");
-/*
+
 app.MapPut("/suppliers/{id}", async (
     ApplicationDbContext context, Guid id, Supplier supplier) =>
     {
-        if (id != supplier.Id)
-        {
-            return Results.BadRequest("Id mismatch");
-        }
+        var existingSupplier = await context.Suppliers.AsNoTracking().FirstOrDefaultAsync(s => s.Id == id && s.Active == true);
+        if (existingSupplier is null) return Results.NotFound();
 
-        context.Entry(supplier).State = EntityState.Modified;
-        await context.SaveChangesAsync();
-        return Results.NoContent();
+        if (!MiniValidator.TryValidate(supplier, out var errors))
+            return Results.ValidationProblem(errors);
+
+        context.Update(supplier);
+        var result = await context.SaveChangesAsync();
+
+        return result > 0
+            ? Results.Ok(supplier)
+            : Results.BadRequest("An error occurred while saving the record");
     })
+    .ProducesValidationProblem()
+    .Produces<Supplier>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status404NotFound)
     .WithName("UpdateSupplier")
     .WithTags("Suppliers");
-*/
+
 app.MapDelete("/suppliers/{id}", async (
     ApplicationDbContext context, Guid id) =>
     {
         var supplier = await context.Suppliers.FirstOrDefaultAsync(s => s.Id == id && s.Active == true);
-        if (supplier is null)
-        {
-            return Results.NotFound();
-        }
+        if (supplier is null) return Results.NotFound();
 
         context.Suppliers.Remove(supplier);
-        await context.SaveChangesAsync();
-        return Results.NoContent();
+        var result = await context.SaveChangesAsync();
+        return result > 0
+            ? Results.NoContent()
+            : Results.BadRequest("An error occurred while deleting the record");
     })
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .Produces(StatusCodes.Status404NotFound)
     .WithName("DeleteSupplier")
     .WithTags("Suppliers");
 
