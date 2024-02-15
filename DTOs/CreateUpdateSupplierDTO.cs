520f1f9c49411f3023b5bfad97ca34d9fae87445
Kayki Letieri <kaykiletieri37@gmail.com>
2024-02-15 10:02:10 -0300

    feat: add initial dtos

---
+++ DTOs/CreateUpdateSupplierDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace DemoMinimalAPI.DTOs;
+
+public class CreateUpdateSupplierDTO
+{
+    public required string Name { get; set; }
+    public required string Document { get; set; }
+}
---
+++ DTOs/SupplierResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoMinimalAPI.DTOs;
+
+public class SupplierResponseDTO
+{
+    public required long Id { get; set; }
+    public required string Name { get; set; }
+    public required string Document { get; set; }
+}
